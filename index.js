8841f3c97a66dc73c64ec6bb491e4b96853122b5
Shaun Warman <shaunwarman1@gmail.com>
2020-05-23 01:41:07 -0500

    chore: accept cookie options

--- index.js
+++ index.js
@@ -65,21 +65,6 @@ class Web {
         ? process.env.SESSION_KEYS.split(',')
         : ['lad'],
       cookiesKey: process.env.COOKIES_KEY || 'lad.sid',
-      // <https://github.com/pillarjs/cookies#cookiesset-name--value---options-->
-      // <https://github.com/koajs/generic-session/blob/master/src/session.js#L32-L38>
-      cookies: {
-        httpOnly: true,
-        path: '/',
-        overwrite: true,
-        signed: true,
-        maxAge: 24 * 60 * 60 * 1000,
-        secure: process.env.WEB_PROTOCOL === 'https',
-        // we use SameSite cookie support as an alternative to CSRF
-        // <https://scotthelme.co.uk/csrf-is-dead/>
-        // 'strict' is ideal, but would cause issues when redirecting out
-        // for oauth flows to github, google, etc.
-        sameSite: 'lax'
-      },
       livereload: {
         port: process.env.LIVERELOAD_PORT
           ? parseInt(process.env.LIVERELOAD_PORT, 10)
